acfc7060627c8994b41312d6cdc3330b08406e12
Somya Bansal <somi.bansal159@gmail.com>
2023-03-08 20:50:54 +0530

    added outline pass for selected objects

--- src/app/MyComponents/detector/detector.component.ts
+++ src/app/MyComponents/detector/detector.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import * as THREE from 'three';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { RenderPass, EffectComposer, OutlinePass } from "three-outlinepass"
 
 @Component({
   selector: 'app-detector',
@@ -9,7 +10,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
   styleUrls: ['./detector.component.css']
 })
 export class DetectorComponent {
-  loader: OBJLoader;
+  loader: GLTFLoader;
   scene: THREE.Scene;
   canvas: HTMLCanvasElement;
   renderer: THREE.WebGLRenderer;
@@ -17,31 +18,44 @@ export class DetectorComponent {
   sizes = {
     width: window.innerWidth,
     height: window.innerHeight
-  }
+  };
+  
+  pointer = new THREE.Vector2(1,1);
+  raycaster = new THREE.Raycaster();
   
 
   @ViewChild('myCanvas', {static: true}) myCanvas: ElementRef<HTMLCanvasElement>;
 
   constructor() {
+    // Scene
     this.scene = new THREE.Scene();
 
-    this.loader = new OBJLoader();
-    this.loader.load( "../../assets/OBJ/Pix.obj" ,  
-                      ( obj ) => { this.scene.add( obj ); } ,
-                      function ( xhr ) { console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' ); } ,
-                      function ( error ) { console.error( error ); }
-                    )
+    // Load gltf
+    this.loader = new GLTFLoader();
+    this.loader.load( "../../assets/models/cms.gltf" ,  
+                      ( gltf ) => { this.scene.add ( gltf.scene ) ; } ,
+                      ( xhr ) => { console.log ( ( xhr.loaded / xhr.total * 100 ) + '% loaded' ) ; },
+                      ( error ) => { console.error ( error ) ; }
+                    );
 
     // Light
     const light = new THREE.AmbientLight(0xffffff, 0.5);
     this.scene.add(light);
 
     // Camera
-    this.camera = new THREE.PerspectiveCamera(45, this.sizes.width/this.sizes.height, 0.1, 10000);
-    this.camera.position.z = 3000;
-    this.camera.position.y = 1500;
-    this.camera.position.x = 1500;
+    this.camera = new THREE.PerspectiveCamera(45, this.sizes.width/this.sizes.height);
+    this.camera.position.z = 30;
     this.scene.add(this.camera);
+
+    window.addEventListener( 'click', (event) => {
+      this.pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+      this.pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+      this.raycaster.setFromCamera( this.pointer, this.camera );
+      const intersects = this.raycaster.intersectObjects( this.scene.children );
+      if(intersects.length > 0)
+          this.outline(intersects[0]);
+    } );
+
   }
 
   ngAfterViewInit() {
@@ -59,6 +73,31 @@ export class DetectorComponent {
       this.renderer.render(this.scene, this.camera);
       window.requestAnimationFrame(loop);
     }
+
+    loop();
+  }
+
+  outline(intersect) {
+    const selectedObjects = [intersect.object];
+
+    const compose = new EffectComposer(this.renderer);
+    const outlinePass = new OutlinePass(new THREE.Vector2(window.innerWidth, window.innerHeight), this.scene, this.camera, selectedObjects);
+
+    outlinePass.renderToScreen = true;
+    outlinePass.selectedObjects = selectedObjects;
+
+    compose.addPass(new RenderPass(this.scene, this.camera));
+    compose.addPass(outlinePass);
+
+    outlinePass.edgeStrength = 4;
+    outlinePass.visibleEdgeColor.set(0xffffff);
+    outlinePass.hiddenEdgeColor.set(0xffffff);
+
+    const loop = () => {
+      compose.render(this.scene, this.camera);
+      window.requestAnimationFrame(loop);
+    }
+
     loop();
   }
 }
